9c1b406466b966f8922d8c147e393b65b7075a0a
shammasktl <mshammast370737@gmail.com>
2024-09-12 17:57:21 +0530

    Active Added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 import './Navbar.css'
 
 const Navbar = () => {
-    const [active, setActive] = useState('home')
+    const [ menu,setMenu ] = useState('home')
   return (
     <div className='navbar'>
       <div className="logo">
@@ -10,13 +11,13 @@ const Navbar = () => {
       </div>
       <div className="links">
         <ul>
-            <li ><a href="">Home</a></li>
-            <li><a href="">Products</a></li>
-            <li><a href="">About</a></li>
+            <li className={menu==='home'?'active':''} onClick={()=>{setMenu('home')}}>Home</li>
+            <li className={menu==='products'?'active':''} onClick={()=>{setMenu('products')}}>Products</li>
+            <li className={menu==='about'?'active':''} onClick={()=>{setMenu('about')}}>About</li>
         </ul>
       </div>
       <div className="cart">
-      <i class='bx bxs-shopping-bag'></i>
+      <i className='bx bxs-shopping-bag'></i>
       <div className="dot"></div>
       </div>
     </div>
